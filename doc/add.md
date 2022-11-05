f610128420da2f0633cd8bbdebabe875c4d8df99
tmparks <tmparks@yahoo.com>
2022-11-05 14:08:12 -0400

    add user documentation

---
+++ doc/add.md
@@ -0,0 +1,34 @@
+
+# Add contacts
+
+Create posts, one for each contact.
+
+* Create a new post from the dashbord.
+
+  ![Make a post](post-1.png)
+
+* Set the title of the post to the person's name.
+* Add their photo as the post's featured image.
+
+  ![Add title and image](post-2.png)
+
+* Upload a photo.
+* Select it as the featured image.
+* Enter a link as the image's title.
+  For example:
+  - Text Message: `sms:+1-315-555-0123`
+  - Phone: `tel:+1-315-555-0123`
+  - Facetime Audio: `facetime-audio:user@example.com`
+  - Facetime Video: `facetime:user@example.com`
+  - Skype Audio: `skype:echo123`
+  - Skype Video: `skype:echo123?calll&video=true`
+  - Zoom Meeting: `https://us05web.zoom.us/j/123456789`
+
+  ![Configure image](post-3.png)
+
+* Publish the post.  
+    
+  ![Publish post](post-4.png)
+
+## Next
+[Organize contacts](organize.md) (optional)
--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php get_header(); ?>
 
-<h1>Categories</h1>
+<h1>Web Apps</h1>
 <?php echo get_the_category_list(); ?>
 
-<h1>Geting Started</h1>
-<p>This theme displays a grid of icons.</p>
-<p>Create posts, one for each icon. The title of a post is displayed below its icon.</p>
-<p>Upload a featured image for each post. The featured image is used as the icon for a post.</p>
-<p>Set a title for each featured image. The title is used as a link when the icon is tapped.</p>
+<hr/>
 
-<h1>Advanced Options</h1>
-<p>Navigate to <strong>Appearance > Customize > Site Identity</strong> and upload a site icon.</p>
-<p>Assign each post to one or more categories. A grid is created for each category.</p>
+<p>
+<a href="https://github.com/tmparks/elder/blob/main/README.md">Documentation</a>
+&bull;
+<a href="/wp-admin/">Dashboard</a>
+&bull;
+<a href="/wp-admin/edit.php">Posts</a>
+&bull;
+<a href="/wp-admin/upload.php">Photos</a>
+&bull;
+<a href="/wp-admin/edit-tags.php?taxonomy=category">Categories</a>
+&bull;
+<a href="/wp-admin/customize.php">Appearance</a>
+&bull;
+<a href="/wp-admin/users.php">Users</a>
+</p>
 
 <?php get_footer(); ?>
